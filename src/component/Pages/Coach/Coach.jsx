c8c19b9024755edd0bc561da8b41e02184453da4
Vandy <matthew.vandergrift@homeaidepi.com>
2022-10-04 18:54:25 -0400

    feat: updated public link for about me.

--- src/component/Pages/Coach/Coach.jsx
+++ src/component/Pages/Coach/Coach.jsx
@@ -8,7 +8,7 @@ class Coach extends Component {
 					<div className="container"> 
 						<div className="row justify-content-center text-center">
 							<div className="col-12">
-							<iframe src="https://netorgft990687-my.sharepoint.com/personal/me_vandy_io/_layouts/15/Doc.aspx?sourcedoc={24467b86-0739-4b25-968b-aa373c430803}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="1024px" height="760px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+							<iframe src="https://netorgft990687-my.sharepoint.com/:p:/g/personal/me_vandy_io/EYZ7RiQ5ByVLlouqNzxDCAMBSJD9CpYaZ1tUF_7C0Owz-A?e=Zoicbt" width="1024px" height="760px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
 							</div> 
 						</div>
 					</div>
