dc1777bc919a9a272a6b61f54a4f5b6a26d18040
Vandy <matthew.vandergrift@homeaidepi.com>
2023-01-02 13:10:26 -0500

    feat: update coach bio

--- src/component/Pages/Coach/Coach.jsx
+++ src/component/Pages/Coach/Coach.jsx
@@ -8,10 +8,21 @@ class Coach extends Component {
 					<div className="container"> 
 						<div className="row justify-content-center text-center">
 							<div className="col-12">
-							<iframe src="https://netorgft990687-my.sharepoint.com/:p:/g/personal/me_vandy_io/EYZ7RiQ5ByVLlouqNzxDCAMBSJD9CpYaZ1tUF_7C0Owz-A?e=Zoicbt" width="1024px" height="760px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+							<iframe src="https://netorgft990687-my.sharepoint.com/personal/me_vandy_io/_layouts/15/Doc.aspx?sourcedoc={11ac3b24-684f-4660-bc8d-eb259cdda64d}&amp;action=embedview&amp;wdAr=1.7777777777777777" width="640px" height="480px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> presentation, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
 							</div> 
 						</div>
 					</div>
+
+					<a style={{alignContent:"center"}} href='https://netorgft990687-my.sharepoint.com/:b:/g/personal/me_vandy_io/EWcCoB5ulbJGk_bgbOC9r2UB1IMC0dWg3Ur6tBWQS2pXRA?e=wLz2WP'> <h1>My Resume</h1> </a>
+					<div className="container"> 
+						<div className="row justify-content-center text-center">
+							<div className="col-12">
+							<iframe src="https://netorgft990687-my.sharepoint.com/personal/me_vandy_io/_layouts/15/embed.aspx?UniqueId=1ea00267-956e-46b2-93f6-e06ce0bdaf65" width="760px" height="1024px" frameborder="0" scrolling="no" allowfullscreen title="CV-Matthew-Vandergrift.pdf"></iframe>
+							</div> 
+						</div>
+					</div>
+
+					
 				</section>
 			</div>
         )
